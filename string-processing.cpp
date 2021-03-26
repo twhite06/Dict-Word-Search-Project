88ce71ad6f1e5f38177ca2f5e049cf62d9edaa2b
twhite06 <50536683+twhite06@users.noreply.github.com>
2021-03-26 15:16:55 -0400

    Create string-processing.cpp

---
+++ string-processing.cpp
@@ -0,0 +1,32 @@
+/*
+ * stringProcessing.cpp
+ * COMP15
+ * Spring 2019
+ *
+ *
+ * Author: Tim White 
+ * Date: 11/15/2019
+ */
+#include "stringProcessing.h"
+#include <string>
+#include <cctype>
+#include <iostream>
+
+using namespace std;
+
+
+string stripNonAlphaNum(string input){
+    int size = input.size();
+    int i = 0, first = 0, last = 0;
+    while(!isalnum(input[i])){
+        i++;
+    }
+    first = i;
+    i = size - 1;
+    while (!isalnum(input[i])){
+        i--;
+    }
+    last = i;
+    input = input.substr(first, (last-first) + 1);
+    return input;
+}
